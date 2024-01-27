0b886534d9c88b4b717ba26c1f35c53db53aa4fd
bruizk <beruizk@gmail.com>
2024-01-27 12:31:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Flex, Layout } from "antd";
+import { Route, Router, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NewMatchPage from './pages/NewMatchPage';
+import LastMatchesPage from './pages/LastMatchesPage';
+import Navbar from './components/Navbar';
+
+const { Content, Header } = Layout;
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Header style={{ height: 80 }}>
+          <Navbar /> 
+        </Header>
+        <Content>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/last-matches" element={<LastMatchesPage />} />
+            <Route path="/new-match" element={<NewMatchPage />} />
+          </Routes>
+        </Content>
+      </Layout>
     </div>
   );
 }
